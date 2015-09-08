80b88cad461b3f14f19bbdff72535ccc5c96f176
itgsod-keryakous-jumaa <keryakous.jumaa@elev.it-gymnasiet.se>
2015-09-08 10:07:05 +0200

    postoffice to filip

--- postoffice/calculator.py
+++ postoffice/calculator.py
@@ -11,3 +11,30 @@ def is_envelop(width,length,thickness):
     #code ....
 
     return True
+
+
+# START
+
+def weight(vikt):
+    if vikt > 2 or vikt <= 0:
+        return False
+    else:
+        return True
+
+def meassurements(lenght, width, thickness):
+    if lenght >= 14 and lenght <= 60 and width >= 9 and lenght + width + thickness <= 90 and lenght + width + thickness > 0:
+        return True
+    else:
+        return False
+
+
+
+def  fetch_values(vikt,lenght, width, thickness):
+    grillkorv = weight(vikt)
+    ketchup = meassurements(lenght, width, thickness)
+    if grillkorv == True and ketchup == True:
+        return True
+    else:
+        return False
+
+
